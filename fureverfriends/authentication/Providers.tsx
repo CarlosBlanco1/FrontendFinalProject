5aaf51fccb1f3377ad6bd6f2b0d173fca94cfab7
CarlosBlanco1 <97683431+CarlosBlanco1@users.noreply.github.com>
2024-11-13 00:30:11 -0700

    forgot to change redirect uri

--- fureverfriends/authentication/Providers.tsx
+++ fureverfriends/authentication/Providers.tsx
@@ -6,8 +6,8 @@ import { AuthProvider, AuthProviderProps } from "react-oidc-context";
 const oidcConfig : AuthProviderProps = {
   authority: "https://auth.snowse.duckdns.org/realms/advanced-frontend/",
   client_id: "carlos-client",
-//   redirect_uri: "https://carlos-final.duckdns.org/",
-  redirect_uri: "http://localhost:3000/",
+  redirect_uri: "https://carlos-final.duckdns.org/",
+  // redirect_uri: "http://localhost:3000/",
   automaticSilentRenew: true,
   onSigninCallback: async (user: any) => {
     const newUrl = window.location.href.split("?")[0];
